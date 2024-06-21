c4a7597209cd1c2126bab38d6d90c91a25608c5f
kenser11547 <kenser11547.koffy140221@gmail.com>
2024-06-21 09:20:07 -0600

    cambio de post

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
-// Middleware para servir archivos estáticos (si es necesario)
+// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,7 +19,63 @@ app.post('/submit', (req, res) => {
   const { name, message } = req.body;
   console.log('Recibida solicitud POST en la ruta /submit');
   console.log('Datos recibidos:', name, message);
-  res.send(`<h1>Gracias, ${name}</h1><p>Tu mensaje: ${message}</p><button onclick="window.history.back()">Regresar</button>`);
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Gracias</title>
+      <style>
+        body {
+          font-family: 'Arial', sans-serif;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          background: linear-gradient(135deg, #72EDF2 10%, #5151E5 100%);
+          color: #333;
+        }
+        .container {
+          background: rgba(255, 255, 255, 0.85);
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          text-align: center;
+          width: 90%;
+          max-width: 400px;
+        }
+        h1 {
+          margin-bottom: 20px;
+          font-size: 24px;
+          color: #333;
+        }
+        p {
+          font-size: 18px;
+        }
+        button {
+          padding: 10px;
+          background-color: #007BFF;
+          color: white;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+        }
+        button:hover {
+          background-color: #0056b3;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Gracias, ${name}</h1>
+        <p>Tu mensaje: ${message}</p>
+        <button onclick="window.history.back()">Regresar</button>
+      </div>
+    </body>
+    </html>
+  `);
 });
 
 // Escucha en el puerto especificado
